b42e8e1486308b170d9ece932b44c38c1f33c347
Tony <franciscoantoniogarciaquintana@gmail.com>
2021-03-20 20:48:41 -0700

    added AutomatedTesting project

---
+++ AutomatedTesting/FirstTestExample.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace AutomatedTesting
+{
+    [TestFixture]
+    public class FirstTestExample
+    {
+        IWebDriver driver;
+        ChromeOptions options;
+        string urlProject = "";
+
+        [SetUp]
+        public void Initialize() {
+            options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            driver = new ChromeDriver(options);
+
+            //set url project;
+            urlProject = "https://localhost:44300/#/dashboard";
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            //go to dashboard project
+            driver.Navigate().GoToUrl(urlProject);
+            Thread.Sleep(5000);
+            driver.Quit();
+            Assert.True(true);
+
+        }
+    }
+}
---
+++ AutomatedTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomatedTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomatedTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc60bb8a-1cef-47bb-ad12-bd65bf48ed43")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
